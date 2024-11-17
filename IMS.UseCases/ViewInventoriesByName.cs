1a0e4abfe8fc80d3ef22935f35c881f82d9f2406
Rifat <khrifatamin@gmail.com>
2024-11-17 17:46:35 +0600

    Add project files.

---
+++ IMS.UseCases/ViewInventoriesByName.cs
@@ -0,0 +1,19 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.UseCases
+{
+    public class ViewInventoriesByName
+    {
+        public readonly IInventoryRepository _inventoryRepository;
+        public ViewInventoriesByName(IInventoryRepository inventoryRepository)
+        {
+             this._inventoryRepository = inventoryRepository;
+        }
+        public async Task<IEnumerable<Inventory>> ExecuteAsync(string name)
+        {
+           return await _inventoryRepository.GetInventoriesByName(name);
+
+        }
+    }
+}
